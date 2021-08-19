6d00e6c1ba8be8dc94fe829f54f901b72caacdf3
troe5906 <troe5906@CV-BB-5768>
2021-08-19 09:38:10 +0200

    Add project files.

---
+++ EX01HelloWorld/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace EX01HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            String schoolName; 
+            schoolName = "AspIT-Nordjylland";
+            
+            Console.WriteLine(schoolName);
+
+            String motto;
+            motto = "Fokuseret Talent";
+
+            Console.Write(schoolName);
+            Console.Write(" ");
+            Console.WriteLine(motto);
+            Console.WriteLine($"{schoolName} er en skole i Aalborg. Mottoet er {motto}");
+            
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
